f0daffe7bb1ad24b1711ccf5d0cb0ece8d4bc2e1
Martha Kerr <mjlkerr77@gmail.com>
2020-10-23 15:28:14 -0400

    add homescreen, start structure

---
+++ src/home/components/HomeScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import {View, Text} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
